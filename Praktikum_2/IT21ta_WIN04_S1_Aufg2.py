b867c526ae1286224226ee1971a064820305b4d0
Adimoser <adrian.moser00@hotmail.com>
2022-09-26 16:53:44 +0200

    write needed methods

--- Praktikum_2/IT21ta_WIN04_S1_Aufg2.py
+++ Praktikum_2/IT21ta_WIN04_S1_Aufg2.py
@@ -2,12 +2,47 @@
 """
 Created on Mon Sep 26 15:09:26 2022
 
-@author: adria
+@author: Adrian Moser
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def IT21ta_WIN_04_S1_Aufg2(a, xmin, xmax):
-    return "das kommt noch"
+#a = np.array([3., -2., 8])
+
+#l = np.size(a)
+
+#a3 = np.ones(l-1)
+#print(a3)
+
+
+def startfunction(a, xmin, xmax):
+    #startfunction a is  an np.array
+    return "needs to be done"
+
+def checkArraySize(a):
+    #a should not be 0 or too big
+    return "needs to be done"
+    
+def createDiraffeArray(a):
+    #creates a new array out of the old array and calculates the new coeffizient
+    return "needs to be done"
+
+def createIndiffineArray(a):
+    #creates a new array out of the old array and calculates the new coeffizient
+    return "needs to be done"
+    
+def createPlotValues(a, x):
+    #calculates the y-Values and adds to the plt.plot
+    return "needs to be done"
+
+def showPlot():
+    #adds the needed elements e.g. grid, labels and shows plot
+    return "needs to be done"
+    
+    
+
+
+
+
