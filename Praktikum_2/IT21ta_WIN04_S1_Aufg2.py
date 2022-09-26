cc7cabbd411cffd48eceb4d5ce360a1d4feaff22
Adimoser <adrian.moser00@hotmail.com>
2022-09-26 15:49:54 +0200

    initialized aufgabe 2

---
+++ Praktikum_2/IT21ta_WIN04_S1_Aufg2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 26 15:09:26 2022
+
+@author: adria
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def IT21ta_WIN_04_S1_Aufg2(a, xmin, xmax):
+    return "das kommt noch"
