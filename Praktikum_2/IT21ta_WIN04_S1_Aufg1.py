9fe027e7d81ffca47c1dba86218f70e7e8f69da1
Adimoser <adrian.moser00@hotmail.com>
2022-09-26 13:36:28 +0200

    added derivate of f

--- Praktikum_2/IT21ta_WIN04_S1_Aufg1.py
+++ Praktikum_2/IT21ta_WIN04_S1_Aufg1.py
@@ -7,16 +7,19 @@ import numpy as np
 def f(x):
     return x**5-5*x**4-30*x**3+110*x**2+29*x-105
 
-#def plotting():
-    #plt.xlim()
-    #plt.ylim()
+def ablf(x):
+    return 5*x**4-20*x**3-90*x**2+220*x+29
 
-x = np.arange(-10, 11, 1)
+def stammF(x):
+    return 
 
-plt.xlim()
-plt.ylim()
+x = np.arange(-10, 11, 0.1)
+
+plt.xlim([-10, 10])
+plt.ylim([-1500,1500])
 
 plt.plot(x, f(x))
+plt.plot(x, ablf(x))
 
 plt.grid()
 
