1a94323e9d8dd92e827605c3faea2cb1b15fb63c
MikeDiethelm <83275601+MikeDiethelm@users.noreply.github.com>
2022-10-02 22:44:45 +0200

    finished ex 2
    -

--- Praktikum_2/IT21ta_WIN04_S1_Aufg2.py
+++ Praktikum_2/IT21ta_WIN04_S1_Aufg2.py
@@ -2,47 +2,43 @@
 """
 Created on Mon Sep 26 15:09:26 2022
 
-@author: Adrian Moser
+@author: Mike Diethelm
 """
-
-import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 
-
-#a = np.array([3., -2., 8])
-
-#l = np.size(a)
-
-#a3 = np.ones(l-1)
-#print(a3)
-
-
-def startfunction(a, xmin, xmax):
-    #startfunction a is  an np.array
-    return "needs to be done"
-
-def checkArraySize(a):
-    #a should not be 0 or too big
-    return "needs to be done"
+  
+def upAndDownDerivate(a, xmin, xmax):
     
-def createDiraffeArray(a):
-    #creates a new array out of the old array and calculates the new coeffizient
-    return "needs to be done"
+    #KoeffizientenVektor
+    a = np.array(a)
+    x = np.linspace(xmin, xmax, 100)
+    f = np.zeros(len(x))
+    downF = np.zeros(len(x))
+    upF = np.zeros(len(x))
 
-def createIndiffineArray(a):
-    #creates a new array out of the old array and calculates the new coeffizient
-    return "needs to be done"
-    
-def createPlotValues(a, x):
-    #calculates the y-Values and adds to the plt.plot
-    return "needs to be done"
+    for n in range(len(a)):
+        f += a[n] * x ** n
+        downF += n * a[n] * x**(n-1)
+        upF += (1/(n+1)) * a[n] * x**(n+1)
 
-def showPlot():
-    #adds the needed elements e.g. grid, labels and shows plot
-    return "needs to be done"
-    
-    
+    return (x, f, downF, upF)
 
 
+x, f, fDown, fUp = upAndDownDerivate([-105, 29, 110, -30, -5, 1], -10, 10)
 
+plt.plot(x, f)
+plt.plot(x, fDown)
+plt.plot(x, fUp)
+plt.xlabel("X-Werte")
+plt.ylabel("Y-Werte")
+plt.title("Funktionen und ihre Ableitungen / Stammfunktionen")
+plt.legend(["Function", "Ableitung", "Stammfunktion"])
+plt.grid(color='lightgray', linestyle='-', linewidth=1)
 
+plt.xlim(right=10)
+plt.xlim(left=-8)
+plt.ylim(top=1500)
+plt.ylim(bottom=-1500)
+
+plt.show()
