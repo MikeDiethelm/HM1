9d93c50bcd4899d37b441ad38e8ff890238c539f
Adimoser <adrian.moser00@hotmail.com>
2022-10-08 15:40:13 +0200

    started number 3

---
+++ Praktikum_3/IT21_WIN04_S2_Aufg3.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 19:48:22 2022
+
+@author: adria
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+def bruch(x):
+    return (outerRoot(x/2) ** 2) / 4
+
+def innerRoot(x):
+    return 2*np.sqrt(1-bruch(x))
+
+def outerRoot(x):
+    if(x == 6):
+        return 1
+    else:
+        return np.sqrt(2 - innerRoot(x))
+
+def calculateCircumfence(x):
+    return x * outerRoot(x)
+
+
+x = 6
+resultetXValues = np.array([])
+resultetYValues = np.array([])
+calculatedResult = 0
+while x <= 500:
+    calculatedResult = calculateCircumfence(x)
+    resultetXValues = np.append(resultetXValues, x)
+    resultetYValues = np.append(resultetYValues, calculatedResult)
+    x = x*2
+    
+plt.plot(resultetXValues, resultetYValues)
+plt.ylabel("Umfang")
+plt.xlabel("Anzahl Ecken")
+plt.show()
