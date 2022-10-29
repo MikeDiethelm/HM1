0d088ad250e9ac33d699aaaa81669d362481a65b
Adimoser <adrian.moser00@hotmail.com>
2022-10-29 19:49:48 +0200

    haha

--- Praktikum 5/WIN04_S5_Aufg3.py
+++ Praktikum 5/WIN04_S5_Aufg3.py
@@ -8,13 +8,15 @@ Created on Sat Oct 29 15:17:55 2022
 import numpy as np
 
 def sekanten(f,x0,x1,tol):
-    y= f(x1)-f(x0)
-    x3= x1-((x1-x0)/y)
-    round(x3,tol)
-    return x3
+    y = np.exp(x1**2)+x1**-3-10 #f(x1)
+    x = np.exp(x0**2)+x0**-3-10 #f(x0)
+    z = y -x
+    x3= x1-((x1-x0)/z)
+    return round(x3,tol)
 
 #example
 def f(x):
+    
     return np.exp(x**2)+x**-3-10
 
 print(sekanten(1, -1.5, -1, 4))
