e2822d383773e925a0cfb00742f654104b39e82d
kolatben <kolatben@students.zhaw.ch>
2022-10-30 14:39:15 +0100

    added answer

--- Praktikum 5/WIN04_S5_Aufg3.py
+++ Praktikum 5/WIN04_S5_Aufg3.py
@@ -6,17 +6,30 @@ Created on Sat Oct 29 15:17:55 2022
 """
 
 import numpy as np
+import math
 
 def sekanten(f,x0,x1,tol):
-    y = np.exp(x1**2)+x1**-3-10 #f(x1)
-    x = np.exp(x0**2)+x0**-3-10 #f(x0)
-    z = y -x
-    x3= x1-((x1-x0)/z)
-    return round(x3,tol)
+    xn = x1
+    xprev = x0
+    while abs(xprev - xn) > tol and abs(xprev-xn) != 0:
+        xnew = xn-(((xn-xprev)/(f(xn)-f(xprev)))*f(xn))
+        xprev = xn
+        xn = xnew
+        print(xnew)
+    return xnew
 
-#example
+#exercise 1
 def f(x):
-    
     return np.exp(x**2)+x**-3-10
 
-print(sekanten(1, -1.5, -1, 4))
+#exercise 2
+def g(x):
+    return x-((-(1/3)*math.pi*(x**3))+(5*math.pi*(x**2)-471))
+
+print("Exercise 1")
+print(sekanten(f,-1.0, -1.2, 0.0001))
+print("Exercise 2")
+print(sekanten(g, 9, 7, 0.0001)) #bitte checken, ob x1 wirklich 7 ist. 
+
+#Fazit: Das Sekantenverfahren ist gegenüber dem Newton-Verfahren einfacher darzustellen, 
+        #da die Ableitung nicht mitgeliefert werden muss.  
