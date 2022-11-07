ca06a0cb88d3702f5279d7f923aa5cf795e50cd1
Adimoser <adrian.moser00@hotmail.com>
2022-11-07 10:43:35 +0100

    commentent print statements

--- Praktikum 6/adrian.py
+++ Praktikum 6/adrian.py
@@ -11,40 +11,40 @@ import matplotlib.pyplot as plt
 def gaus(A, b):
     x_dim = A.shape[0]
     y_dim = A.shape[1]
-    print("x-Dimension ") 
-    print(x_dim)
-    print("y-dimension ")
-    print(y_dim)
+    #print("x-Dimension ") 
+    #print(x_dim)
+    #print("y-dimension ")
+    #print(y_dim)
     round = 0
     while round <= x_dim:
         nextLine = round +1
         while nextLine <= x_dim-1:
-            print(round)
-            print(nextLine)
-            print(A[nextLine][round])
-            print(A[round][round])
+            #print(round)
+            #print(nextLine)
+            #print(A[nextLine][round])
+            #print(A[round][round])
             divideBy = A[nextLine][round] / A[round][round]
-            print("division")
-            print(divideBy)
+            #print("division")
+            #print(divideBy)
             #print(nextLine)
             #print(divideBy)
-            print(b[nextLine][0])
+            #print(b[nextLine][0])
             b[nextLine][0] = b[nextLine][0] - divideBy * b[round][0]
-            print(b[nextLine][0])
+            #print(b[nextLine][0])
             for item in range(y_dim):
-                print("new item")
-                print(item)
-                print(nextLine)
-                print(round)
-                print(A[nextLine][item])
+                #print("new item")
+                #print(item)
+                #print(nextLine)
+                #print(round)
+                #print(A[nextLine][item])
                 A[nextLine][item] = A[nextLine][item] - divideBy * A[round][item]
-                print(A[nextLine][item])
+                #print(A[nextLine][item])
             
             nextLine = nextLine +1
         round = round +1
-        print("Result: ")
-        print(A)
-        print(b)
+    print("Result: ")
+    print(A)
+    print(b)
     
     
     
