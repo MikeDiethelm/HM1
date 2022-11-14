80cca2f481433a56f8359653561245a01a284dda
Adimoser <adrian.moser00@hotmail.com>
2022-11-14 10:07:46 +0100

    aufgeraeumt und b + c eingebaut

--- Prakitkum 7/Serie7_Aufg3.py
+++ Prakitkum 7/Serie7_Aufg3.py
@@ -61,20 +61,13 @@ def gaus(A, b):
     #print(b)
     return jordan(A, b)
  
-def polynom(c, arr): 
+def polynom(c, arr):
     return arr[0][0]*c**3+arr[0][1]*c**2+arr[0][2]*c+150
     
 def showPlot(x):
     a = np.arange(0, 15.0, 1)
-    print('Wenn man x = 2')
-    print(np.polyval(x,2))
-    print(polynom(2, x))
-    
     plt.xlim(-5, 15)
-    plt.plot(a, x)
-    print('polyfit')
-    print(np.polyval(x, 3))
-    #plt.plot(np.transpose(x), a)
+    plt.plot(polynom(a, x))
     
 def startMethod():
     Matrix_A = np.array([[0,0,0,1], [2**3,2**2,2,1], [9**3,9**2,9,1], [13**3,13**2,13, 1]])
@@ -94,5 +87,29 @@ def startMethod():
     
     
 
-#Aufgabe 2 (startMethod):
-startMethod()
+#Aufgabe 3: a)
+Matrix_A = np.array([[0,0,0,1], [2**3,2**2,2,1], [9**3,9**2,9,1], [13**3,13**2,13, 1]])
+Vector_b = np.array([[150, 104, 172, 152]])
+Vector_b = np.transpose(Vector_b)
+print("Start matrix: ")
+print(Matrix_A)
+print("start B: ")
+print(Vector_b)
+
+y = np.linalg.solve(Matrix_A, Vector_b)
+print('Gefundenes x-Vektor')
+print(y)
+showPlot(y.transpose())
+
+#Aufgabe 3: b)
+#Anhand der Grafik würden wir sagen dass der Wert x= 6 (2003) 
+#einen y-Wert von ungefähr y= 130 bekommt.
+#Bei x=7 (2004) hat y einen Wert von ungefähr 135.
+
+#Aufgabe 3: c)
+print('Wenn man x = 6 einsetzt: ')
+x6 = polynom(6, y.transpose())
+print('y = ' + str(x6))
+print('Wenn man den Wert 7 einsetzt: ')
+x7 =  polynom(7, y.transpose())
+print('y = '+ str(x7) )
