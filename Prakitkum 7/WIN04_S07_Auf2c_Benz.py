a46efb2ae7c4f71e1737fb8332798c4d85961a2c
kolatben <kolatben@students.zhaw.ch>
2022-11-14 08:19:47 +0100

    finished 2c

--- Prakitkum 7/WIN04_S07_Auf2c_Benz.py
+++ Prakitkum 7/WIN04_S07_Auf2c_Benz.py
@@ -4,16 +4,15 @@ Created on Mon Nov 14 06:55:29 2022
 
 @author: Benssy
 """
-import scipy as sp
+from scipy.linalg import lu
 import numpy as np
 
-#not finished yet
-
-def polynomfunction(x):
-    return sp.linalg.lu(x)
 
 Matrix_A = np.array([[0.8,2.2,3.6],[2.0,3.0,4.0],[1.2,2.0,5.8]])
 Vector_b = np.array([[2.4, 1.0, 4.0]])
 Vector_b = np.transpose(Vector_b)
+p,l,u = lu(Matrix_A)
 
-print(polynomfunction(A,b))
+print(p,l,u)
+
+#Answer: same as in "written form"
